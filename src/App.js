94199edd2521f208da197ae23a82919879e703df
David Bernadi <davidbernadi13@gmail.com>
2021-02-08 16:03:00 +0700

    wip local

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {CardList} from './component/card-list/card-list.component';   
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  //menset default value
+  constructor(){
+    super();
+
+    this.state ={
+      monsters: []
+    };
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters : users}))
+    .catch(error => console.log(error));
+  }
+  render(){
+    return( 
+      <div className="App">
+       <input />
+        <CardList monsters={this.state.monsters} />
+      </div>
+    )
+  }
 }
+
 
 export default App;
