06089cd07ede57155cb4643015414e929701dee1
David Bernadi <davidbernadi13@gmail.com>
2021-02-09 11:03:01 +0700

    Updates

---
+++ precache-manifest.9d9760862990dc115fa6193c2b7ba82f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e74a6988b60e47f73c984b5cbba854d0",
+    "url": "/reactbelajar/index.html"
+  },
+  {
+    "revision": "4502a2274dd39819bfbe",
+    "url": "/reactbelajar/static/css/main.aebb7207.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/reactbelajar/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactbelajar/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4502a2274dd39819bfbe",
+    "url": "/reactbelajar/static/js/main.829ee457.chunk.js"
+  },
+  {
+    "revision": "22a7c781c38d6fb9341c",
+    "url": "/reactbelajar/static/js/runtime-main.7d4e0b0e.js"
+  }
+]);
