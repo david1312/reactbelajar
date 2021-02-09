58b1aab490a2a8311f961754aeaaad267ae91dc9
David Bernadi <davidbernadi13@gmail.com>
2021-02-09 10:39:06 +0700

    add component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import {CardList} from './component/card-list/card-list.component';   
+import {SearchBox} from './component/search-box/search-box.component'
 
 class App extends React.Component{
   //menset default value
@@ -9,20 +10,36 @@ class App extends React.Component{
     super();
 
     this.state ={
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
+
+    // this.handleChange = this.handleChange.bind(this); //bind adalah method in any function that return new function where the context of this is set to whatever we passed to it. 
   }
+  
   componentDidMount(){
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(users => this.setState({monsters : users}))
     .catch(error => console.log(error));
   }
+
+  handleChange = (e) => {
+    this.setState({ searchField:  e.target.value });
+  }
+
   render(){
+    const {monsters, searchField} = this.state;
+    const filteredMonster = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
     return( 
       <div className="App">
-       <input />
-        <CardList monsters={this.state.monsters} />
+        <SearchBox
+          placeholder='cari monster cuy'
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonster}/>
       </div>
     )
   }
